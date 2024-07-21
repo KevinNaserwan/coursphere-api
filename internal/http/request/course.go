bd52196380b7eaeef7316c0de0f8c59979ac242d
kevinnaserwan <kevinnaserwan@gmail.com>
2024-07-21 22:24:01 +0700

    Feat: adjust mentor response http

--- internal/http/request/course.go
+++ internal/http/request/course.go
@@ -1,6 +1,17 @@
 package request
 
 type CreateCourseRequest struct {
+	BannerImage string `json:"banner_image"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	MentorID    string `json:"mentor_id"`
+	Star        int    `json:"star"`
+	Price       int    `json:"price"`
+	Lessons     int    `json:"lessons"`
+	CategoryID  string `json:"category_id"`
+}
+
+type UpdateCourseRequest struct {
 	BannerImage      string `json:"banner_image"`
 	Title            string `json:"title"`
 	Description      string `json:"description"`

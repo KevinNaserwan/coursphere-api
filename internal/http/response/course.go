af70687b886e3334d9ee84dee85de4a6c123f9e4
kevinnaserwan <kevinnaserwan@gmail.com>
2024-07-21 22:24:59 +0700

    Feat: adjust course api

--- internal/http/response/course.go
+++ internal/http/response/course.go
@@ -1,13 +1,14 @@
 package response
 
 type CourseResponse struct {
-	BannerImage string                   `json:"banner_image"`
-	Title       string                   `json:"title"`
-	Description string                   `json:"description"`
-	Star        int                      `json:"star"`
-	Price       int                      `json:"price"`
-	Lessons     int                      `json:"lessons"`
-	Mentor      []MentorResponse         `json:"mentor"`
-	Category    []CourseCategoryResponse `json:"category"`
-	Videos      []VideoResponse          `json:"videos"`
+	ID          string                 `json:"id"`
+	BannerImage string                 `json:"banner_image"`
+	Title       string                 `json:"title"`
+	Description string                 `json:"description"`
+	Star        int                    `json:"star"`
+	Price       int                    `json:"price"`
+	Lessons     int                    `json:"lessons"`
+	Mentor      MentorResponse         `json:"mentor"`
+	Category    CourseCategoryResponse `json:"category"`
+	Videos      []VideoResponse        `json:"videos"`
 }
